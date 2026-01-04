cf28f7694894f4b2c46e05d63d7320c863bc6792
Om Mishra <contact@om-mishra.com>
2026-01-05 03:27:29 +0530

    Solved 'quickest concat' (python3)

--- Dynamic-Programming/114-quickest-concat/quickest-concat.py
+++ Dynamic-Programming/114-quickest-concat/quickest-concat.py
@@ -2,6 +2,8 @@ def quickest_concat(s, words):
   return _quickest_concat(s, words, {})
 
 def _quickest_concat(s, words, cache):
+
+
   if s in cache:
     return cache[s]
 
@@ -12,11 +14,13 @@ def _quickest_concat(s, words, cache):
 
   for word in words:
     if s[:len(word)] == word:
-      quickest_concat = min(quickest_concat, 1 + _quickest_concat(s[len(word):], words, cache))
-
+      result = _quickest_concat(s[len(word):], words, cache)
+      if result != -1:
+        quickest_concat = min(quickest_concat, 1 + result)
 
   if quickest_concat == float("inf"):
     cache[s] = -1
     return -1
+
   cache[s] = quickest_concat
   return quickest_concat
